9e9fba5a4dba5426baadcc46f8f292348cf7e60e
Daniel Hindrikes <daniel@hindrikes.se>
2020-04-07 10:27:26 +0200

    initial commit

---
+++ CollectionViewDisposeSample/MainShell.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CollectionViewDisposeSample
+{
+    public partial class MainShell
+    {
+        public MainShell()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            GoToAsync("//photos?id=1");
+        }
+
+        void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            GoToAsync("//photos?id=2");
+        }
+    }
+}
---
+++ CollectionViewDisposeSample/PhotosPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CollectionViewDisposeSample
+{
+    public partial class PhotosPage
+    {
+        public PhotosPage()
+        {
+            InitializeComponent();
+        }
+
+        public ObservableCollection<string> Items { get; set; }
+
+        public void LoadData()
+        {
+            var images = new List<string>();
+
+
+                images.Add("photo_1.jpg");
+                images.Add("photo_2.jpg");
+                images.Add("photo_3.jpg");
+                images.Add("photo_4.jpg");
+                images.Add("photo_5.jpg");
+
+
+            Items = new ObservableCollection<string>(images);
+            Photos.ItemsSource = Items;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            LoadData();
+        }
+
+        public int PageId
+        {
+            get;set;
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Shell.Current.GoToAsync("//main");
+        }
+    }
+}
